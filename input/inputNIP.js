07dd764d2ef2206c391cfe0cf2bf75fa4e62bbca
Pedro Martyns <124056994+OPedromartyns@users.noreply.github.com>
2024-01-09 20:45:32 -0300

    add

---
+++ input/inputNIP.js
@@ -0,0 +1,4 @@
+var nome = prompt("Por favor digite seu nome ")
+var idade = prompt("Por favor digite sua idade ")
+var profissão =prompt ("Qual sua profissão?")
+alert(`Olá, ${nome}! voce tem ${idade} anos de idade tenho certeza que voce é um otimo ${profissão} Bem-vindo!`); 
---
+++ input/somandodoisnumeros.js
@@ -0,0 +1,9 @@
+var num1 = prompt ("Digite um numero para somar")
+var num2 = prompt ("Digite outro numero")
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+var resultado = num1 + num2;
+
+alert(`A soma de ${num1} e ${num2} e ${resultado}`)
